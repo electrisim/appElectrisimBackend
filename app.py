73bbeaabd3b1028e9bf6dabb056ffb36358a67b4
kglikk <kglikk@gmail.com>
2024-01-02 14:01:28 +0100

    last rows removed

--- app.py
+++ app.py
@@ -920,8 +920,8 @@ def simulation():
                 return response 
         
 #dla produkcji usuwaj te wiersze          
-if __name__ == '__main__':
+#if __name__ == '__main__':
     #app.debug = False
     #app.run(host = '127.0.0.1', port=5005)
-    app.debug = True
-    app.run(host = '127.0.0.1', port=5000)
+#    app.debug = True
+#    app.run(host = '127.0.0.1', port=5000)
