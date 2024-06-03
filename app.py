e43a4e63543152591d8aceaa608c5a6638827a2b
kglikk <kglikk@gmail.com>
2024-06-03 14:22:11 +0200

    added openDSS

--- app.py
+++ app.py
@@ -85,5 +85,6 @@ def simulation():
 if __name__ == '__main__':
     #app.debug = False
     #app.run(host = '127.0.0.1', port=5005)
-    app.debug = True
-    app.run(host = '127.0.0.1', port=5000)
+    #app.debug = True
+    #app.run(host = '127.0.0.1', port=5000)
+    app.run(host = '0.0.0.0', port=5000, debug=True)
